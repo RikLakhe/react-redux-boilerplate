5e0155f3a27c809eac536564a0eaac65e9edc98e
Rikesh <rikesh.shrestha@citytech.global>
2019-05-08 16:09:41 +0545

    setup redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
-// import { Provider } from 'react-redux'
+import {Provider} from 'react-redux'
+import store from './store'
 import logo from './images.png'
 import './App.css'
 
@@ -9,13 +10,15 @@ class App extends Component {
 
     render() {
         return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <h1 className="App-title">React/Redux Express Starter</h1>
-            </header>
-        </div>
-    )
+            <Provider store={store}>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h1 className="App-title">React/Redux Express Starter</h1>
+                    </header>
+                </div>
+            </Provider>
+        )
     }
 }
 
